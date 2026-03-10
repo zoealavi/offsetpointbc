7901d3b61e198bf09b86887e38d735627a2371c6
Alavi <zoe.alavi@fraserhealth.ca>
2026-03-10 11:10:06 -0700

    params control min max offset for pop dense analysis

--- R/ave_dist_btw_ppl.R
+++ R/ave_dist_btw_ppl.R
@@ -10,12 +10,14 @@
 #'
 #' @param sf_boundary sf boundary with a column containing population data
 #' @param total_pop_col name of column containing population data
+#' @param min_offset_dis set a minimum offset (1 to 2 times the average distance is standard)
+#' @param max_offset_dis set a maximum offset (3 to 5 times the average distance is standard)
 #'
 #' @return returns an sf boundary object with area, population density, and min/max average distance to offset by
 #'
 #' @examples
 #' \dontrun{
-#'   offset_points_within_boundary(postal_code, bcmaps::health_chsa, "cmnty_hlth_serv_area_code", "chsa", "sf_boundary_total_pop_col")
+#'   offset_points_within_boundary(postal_code, bcmaps::health_chsa, "cmnty_hlth_serv_area_code", "chsa", "total_pop_chsa")
 #'   }
 #'
 #' @export
@@ -23,12 +25,15 @@
 #' @importFrom sf st_area
 #' @importFrom dplyr %>% mutate
 ave_dist_btw_ppl <- function(sf_boundary,
-                             total_pop_col){
+                             total_pop_col,
+                             min_offset_dis = 1,
+                             max_offset_dis = 3
+                             ){
 
   df <- sf_boundary %>%
     mutate(polygon_area = sf::st_area(geometry)) %>%
     mutate(pop_density = .[[total_pop_col]] / .$polygon_area) %>%
-    mutate(min_ave_dist = as.numeric((1 * sqrt(1/pop_density))) + 50) %>% ## buffering by 50m ensures a minimum of 50m offset within calculation
-    mutate(max_ave_dist = as.numeric((3 * sqrt(1/pop_density))) + 50) ## buffering by 50m ensure a minimum of 50m offset within calculation
+    mutate(min_ave_dist = as.numeric((min_offset_dis * sqrt(1/pop_density))) + 50) %>% ## buffering by 50m ensures a minimum of 50m offset within calculation
+    mutate(max_ave_dist = as.numeric((max_offset_dis * sqrt(1/pop_density))) + 50) ## buffering by 50m ensure a minimum of 50m offset within calculation
 
 }
